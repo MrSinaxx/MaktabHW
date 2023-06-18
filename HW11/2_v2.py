5a92ca9946cb762da91877b8c6b2904b692c66f6
MrSinaxx <sinaleylaz@yahoo.com>
2023-06-19 01:40:32 +0330

    Added main with argparse

--- HW11/2_v2.py
+++ HW11/2_v2.py
@@ -5,3 +5,19 @@ def calculate_gpa(grades):
     num_grades = len(grades)
     gpa = total_grades / num_grades
     return gpa
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate GPA')
+    parser.add_argument('-g', '--grades', nargs='+', type=float, help='list of grades')
+    parser.add_argument('-f', '--float', type=int, default=2, help='number of decimal places')
+    args = parser.parse_args()
+
+    grades = args.grades
+    gpa = calculate_gpa(grades)
+
+    float_format = '{:.' + str(args.float) + 'f}'
+    formatted_gpa = float_format.format(gpa)
+    print(formatted_gpa)
+
+if __name__ == '__main__':
+    main()
