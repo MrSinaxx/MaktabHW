d7c73ddcc61455ea34c558d0599897c29e92cac5
MrSinaxx <sinaleylaz@yahoo.com>
2023-06-19 01:47:17 +0330

    Added def count_leap_years

--- HW11/3.py
+++ HW11/3.py
@@ -5,3 +5,9 @@ from jdatetime import datetime as jdatetime
 def calculate_time_difference(start_datetime, end_datetime):
     time_difference = end_datetime - start_datetime
     return time_difference.total_seconds()
+
+def count_leap_years(start_year, end_year):
+    for year in range(start_year, end_year + 1):
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            count += 1
+    return count
