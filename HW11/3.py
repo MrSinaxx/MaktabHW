45ba5c1de8819834d992137750322335f7240028
MrSinaxx <sinaleylaz@yahoo.com>
2023-06-19 01:47:36 +0330

    Added def change_clock

--- HW11/3.py
+++ HW11/3.py
@@ -11,3 +11,9 @@ def count_leap_years(start_year, end_year):
         if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
             count += 1
     return count
+
+def change_clock(start_datetime, end_datetime):
+    if start_datetime.utcoffset() != end_datetime.utcoffset():
+        return "Clock change detected"
+    else:
+        return "No clock change detected"
