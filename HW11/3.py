8ad397363f67dc26e8ca75458aa7f6d1ce8d9c86
MrSinaxx <sinaleylaz@yahoo.com>
2023-06-19 01:46:53 +0330

    Added def calculate_time_difference

--- HW11/3.py
+++ HW11/3.py
@@ -1,2 +1,7 @@
 import datetime
 from jdatetime import datetime as jdatetime 
+
+
+def calculate_time_difference(start_datetime, end_datetime):
+    time_difference = end_datetime - start_datetime
+    return time_difference.total_seconds()
