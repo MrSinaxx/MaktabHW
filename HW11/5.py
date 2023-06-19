36e43a71a31c238fb8f814819e5fbd9202ff74fc
MrSinaxx <sinaleylaz@yahoo.com>
2023-06-19 14:20:52 +0330

    Added main with argparse

--- HW11/5.py
+++ HW11/5.py
@@ -12,3 +12,23 @@ def get_directory_size(directory_path, extension=None):
                 file_path = os.path.join(dirpath, filename)
                 total_size += get_file_size(file_path)
     return total_size
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate the volume (size) of files and folders.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-d', dest='directory', metavar='DIRECTORY', help='calculate the size of a directory')
+    group.add_argument('-f', dest='file', metavar='FILE', help='calculate the size of a file')
+    parser.add_argument('-F', dest='extension', metavar='EXTENSION', help='filter files by extension')
+    
+    args = parser.parse_args()
+    
+    if args.directory:
+        size = get_directory_size(args.directory, args.extension)
+        print(f"{size} KB")
+    elif args.file:
+        size = get_file_size(args.file)
+        print(f"{size} KB")
+
+if __name__ == '__main__':
+    main()
