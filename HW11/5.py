0bfc3fd2e71ccf99a96cedb9e588fbc87c519ff8
MrSinaxx <sinaleylaz@yahoo.com>
2023-06-19 14:15:03 +0330

    Added def get_directory_size

--- HW11/5.py
+++ HW11/5.py
@@ -3,3 +3,12 @@ import argparse
 
 def get_file_size(file_path):
     return os.path.getsize(file_path) // 1024
+
+def get_directory_size(directory_path, extension=None):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(directory_path):
+        for filename in filenames:
+            if extension is None or filename.endswith(extension):
+                file_path = os.path.join(dirpath, filename)
+                total_size += get_file_size(file_path)
+    return total_size
