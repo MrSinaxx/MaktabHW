50164b59c44ce61b3e573da97a1b32b47151dddb
MrSinxx <sinaleylaz@yahoo.com>
2023-06-25 22:20:03 +0330

    Added the neccory imports & url and api key

---
+++ HW12/weather_server.py
@@ -0,0 +1,11 @@
+import http.server
+import json
+import urllib.parse
+import datetime
+from urllib.request import urlopen, HTTPError
+
+url: str = "https://api.openweathermap.org/data/2.5/weather"
+api_key: str = "60be8f85c6c83c4402a1439456f9647c"
+
+
+
