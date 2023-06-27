4a9d90479a695d11cac72c89f367ca1399cb3c3d
MrSinxx <sinaleylaz@yahoo.com>
2023-06-28 01:12:50 +0330

    Added a simple logger

--- HW12/weather_server.py
+++ HW12/weather_server.py
@@ -2,12 +2,25 @@ import http.server
 import json
 import urllib.parse
 import datetime
+import logging
 from urllib.request import urlopen, HTTPError
 from http import HTTPStatus
 from database import WeatherDatabase
 
 url = "https://api.openweathermap.org/data/2.5/weather"
 api_key = "60be8f85c6c83c4402a1439456f9647c"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+log_file = "weather.log"
+file_handler = logging.FileHandler(log_file)
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 database = WeatherDatabase()
 CACHE_EXPIRATION_TIME = datetime.timedelta(minutes=10)
@@ -22,20 +35,23 @@ def handle_request(request):
         response_data = get_database_data()
         if response_data is None:
             request.send_error(HTTPStatus.INTERNAL_SERVER_ERROR, 'Error retrieving database data')
+            logger.error('Error retrieving database data')
         else:
             request.send_response(HTTPStatus.OK)
             request.send_header('Content-Type', 'application/json')
             request.send_header('Access-Control-Allow-Origin', '*')
             request.end_headers()
             request.wfile.write(json.dumps(response_data).encode('utf-8'))
+            logger.info('Data retrieved from the database data')
 
     elif city_name is None or len(city_name) == 0:
         request.send_error(HTTPStatus.BAD_REQUEST, 'Missing city parameter')
+        logger.error('Missing city parameter')
     else:
         city_name = city_name[0]
         cache_data = database.get_response_data(city_name)
 
-        if cache_data is not None and not is_cache_expired(cache_data[2]):# Access cache data using indexes
+        if cache_data is not None and not is_cache_expired(cache_data[2]):
             response_data = {
                 'cache': True,
                 'temperature': cache_data[0],
@@ -43,7 +59,7 @@ def handle_request(request):
                 'last_updated': cache_data[2].strftime('%Y-%m-%d %H:%M:%S')
             }
             
-            print("Reading response from cache")
+            logger.info("Reading response from cache")
             request.send_response(HTTPStatus.OK)
             request.send_header('Content-Type', 'application/json')
             request.send_header('Access-Control-Allow-Origin', '*')
@@ -54,6 +70,7 @@ def handle_request(request):
 
             if response_data is None:
                 request.send_error(HTTPStatus.NOT_FOUND, 'City not found')
+                logger.error('City not found')
                 database.save_request_data("Invalid City", datetime.datetime.now().isoformat())
             else:
                 request.send_response(HTTPStatus.OK)
@@ -66,14 +83,13 @@ def handle_request(request):
 
     database.update_request_counts()
 
+
 def is_cache_expired(last_updated):
     current_time = datetime.datetime.now()
-    last_updated_str = last_updated.strftime('%Y-%m-%d %H:%M:%S')  # Convert datetime object to string
+    last_updated_str = last_updated.strftime('%Y-%m-%d %H:%M:%S')
     cache_time = datetime.datetime.strptime(last_updated_str, '%Y-%m-%d %H:%M:%S')
     elapsed_time = current_time - cache_time
-    return elapsed_time.total_seconds() >= CACHE_EXPIRATION_TIME.total_seconds()  # Compare total seconds
-
-
+    return elapsed_time.total_seconds() >= CACHE_EXPIRATION_TIME.total_seconds()
 
 
 def send_cached_response(request, cache_data):
@@ -100,7 +116,7 @@ def get_city_weather(city_name):
 
     except HTTPError as e:
         error_message = e.read().decode('utf-8')
-        print(f"Error retrieving weather data: {error_message}")
+        logger.error(f"Error retrieving weather data: {error_message}")
 
     return None
 
@@ -122,7 +138,7 @@ def get_database_data():
         return data
 
     except Exception as e:
-        print(f"Error retrieving database data: {str(e)}")
+        logger.error(f"Error retrieving database data: {str(e)}")
 
     return None
 
@@ -136,6 +152,7 @@ def start_server():
     server_address = ('localhost', 8000)
     httpd = http.server.HTTPServer(server_address, WeatherRequestHandler)
     print("Weather server is running on http://localhost:8000")
+    logger.info("Weather server is running on http://localhost:8000")
     httpd.serve_forever()
 
 
