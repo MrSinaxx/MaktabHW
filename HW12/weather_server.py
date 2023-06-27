f8dae87a325ab0aad00f47356875a8cecf1dd605
MrSinxx <sinaleylaz@yahoo.com>
2023-06-27 18:23:55 +0330

    Added parts to def handle_request to for /database

--- HW12/weather_server.py
+++ HW12/weather_server.py
@@ -17,27 +17,34 @@ def handle_request(request):
     query_params = urllib.parse.parse_qs(parsed_url.query)
     city_name = query_params.get('city')
 
-    if city_name is None or len(city_name) == 0:
+    if parsed_url.path == '/database':
+        response_data = get_database_data()
+        if response_data is None:
+            request.send_error(HTTPStatus.INTERNAL_SERVER_ERROR, 'Error retrieving database data')
+        else:
+            request.send_response(HTTPStatus.OK)
+            request.send_header('Content-Type', 'application/json')
+            request.send_header('Access-Control-Allow-Origin', '*')
+            request.end_headers()
+            request.wfile.write(json.dumps(response_data).encode('utf-8'))
+
+    elif city_name is None or len(city_name) == 0:
         request.send_error(HTTPStatus.BAD_REQUEST, 'Missing city parameter')
-        return
+    else:
+        city_name = city_name[0]
+        response_data = get_city_weather(city_name)
 
-    city_name = city_name[0]
-    response_data = get_city_weather(city_name)
-
-    if response_data is None:
-        request.send_error(HTTPStatus.NOT_FOUND, 'City not found')
-
-        database.save_request_data("Invalid City", datetime.datetime.now().isoformat())
-        return
-
-    request.send_response(HTTPStatus.OK)
-    request.send_header('Content-Type', 'application/json')
-    request.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
-    request.end_headers()
-    request.wfile.write(json.dumps(response_data).encode('utf-8'))
-
-    database.save_request_data(city_name, datetime.datetime.now().isoformat())
-    database.save_response_data(city_name, response_data)
+        if response_data is None:
+            request.send_error(HTTPStatus.NOT_FOUND, 'City not found')
+            database.save_request_data("Invalid City", datetime.datetime.now().isoformat())
+        else:
+            request.send_response(HTTPStatus.OK)
+            request.send_header('Content-Type', 'application/json')
+            request.send_header('Access-Control-Allow-Origin', '*')
+            request.end_headers()
+            request.wfile.write(json.dumps(response_data).encode('utf-8'))
+            database.save_request_data(city_name, datetime.datetime.now().isoformat())
+            database.save_response_data(city_name, response_data)
 
 def get_city_weather(city_name):
     try:
@@ -59,15 +66,36 @@ def get_city_weather(city_name):
 
     return None
 
+
+def get_database_data():
+    try:
+        last_hour_requests = database.get_last_hour_requests()
+        city_request_count = database.get_city_request_count()
+
+        data = {
+            'last_hour_requests': last_hour_requests,
+            'city_request_count': city_request_count
+        }
+
+        return data
+
+    except Exception as e:
+        print(f"Error retrieving database data: {str(e)}")
+
+    return None
+
+
 class WeatherRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         handle_request(self)
+
 
 def start_server():
     server_address = ('localhost', 8000)
     httpd = http.server.HTTPServer(server_address, WeatherRequestHandler)
     print("Weather server is running on http://localhost:8000")
     httpd.serve_forever()
+
 
 if __name__ == '__main__':
     start_server()
